9263e7023a9c57f2ccf0adf016f4a27e133627d6
Alexander Psiuk <alex@psiuk.email>
2019-09-21 11:30:21 +0100

    added CanvasGroup Extensions

---
+++ Extensions/UIExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Bytemotiv {
+
+    public static class UIExtensions {
+
+        // --- CanvasGroup
+        public static void Hide(this CanvasGroup canvas) {
+            canvas.alpha = 0.0F;
+            canvas.interactable = false;
+            canvas.blocksRaycasts = false;
+        }
+
+        public static void Show(this CanvasGroup canvas) {
+            canvas.alpha = 1.0F;
+            canvas.interactable = true;
+            canvas.blocksRaycasts = true;
+        }
+
+    }
+
+}
